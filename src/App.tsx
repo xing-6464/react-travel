33c9632e16a62f0ba7252a89dbcc9e35cdbab437
xing-6464 <xing_6464@foxmail.com>
2022-10-20 17:57:48 +0800

    完成页面的header和footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import styles from './App.module.css'
 
-function App() {
+import { Layout, Typography, Input, Menu, Button, Dropdown } from 'antd'
+import { GlobalOutlined } from '@ant-design/icons'
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <div className={styles['App-header']}>
+        {/* top-header */}
+        <div className={styles['top-header']}>
+          <div className={styles.inner}>
+            <Typography.Text>让旅游更幸福</Typography.Text>
+            <Dropdown.Button
+              style={{ marginLeft: 15 }}
+              overlay={
+                <Menu
+                  items={
+                    [
+                      { key: '1', label: '中文'},
+                      { key: '2', label: 'English'}
+                    ]
+                  }
+                />
+              }
+              icon={<GlobalOutlined />}
+            >
+              语言
+            </Dropdown.Button>
+            <Button.Group className={styles['button-group']}>
+              <Button>登录</Button>
+              <Button>注册</Button>
+            </Button.Group>
+          </div>
+        </div>
+        <Layout.Header className={styles['main-header']}>
+          <img src={logo} alt="logo" className={styles['App-logo']} />
+          <Typography.Title level={3} className={styles.title}>React 旅游网</Typography.Title>
+          <Input.Search
+            placeholder={'请输入旅游目的地、主题、或关键字'}
+            className={styles['search-input']}
+          />
+        </Layout.Header>
+        <Menu mode={'horizontal'}
+          className={styles['main-menu']}
+          items={[
+            { key: "1", label: "旅游首页" },
+            { key: "2", label: "周末游" },
+            { key: "3", label: "跟团游" },
+            { key: "4", label: "自由行" },
+            { key: "5", label: "私家团" },
+            { key: "6", label: "邮轮" },
+            { key: "7", label: "酒店+景点" },
+            { key: "8", label: "当地玩乐" },
+            { key: "9", label: "主题游" },
+            { key: "10", label: "定制游" },
+            { key: "11", label: "游学" },
+            { key: "12", label: "签证" },
+            { key: "13", label: "企业游" },
+            { key: "14", label: "高端游" },
+            { key: "15", label: "爱玩户外" },
+            { key: "16", label: "保险" }
+          ]}
+        ></Menu>
+      </div>
+      <Layout.Footer>
+        <Typography.Title level={3} style={{ textAlign: 'center' }}>
+          版权所有 @ React 旅游网
+        </Typography.Title>
+      </Layout.Footer>
     </div>
   );
 }
