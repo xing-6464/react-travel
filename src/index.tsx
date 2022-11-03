de4839f63a597228377e7cbcc848c167f3aa5d04
xing-6464 <xing_6464@foxmail.com>
2022-11-03 10:29:01 +0800

    使用axios异步获取数据

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,13 @@ import ReactDOM from 'react-dom/client'
 import 'antd/dist/antd.min.css'
 import './i18n/configs'
 import { Provider } from 'react-redux'
+import axios from 'axios'
 
 import store from './redux/store'
 import App from './App'
 import './index.css'
+
+axios.defaults.headers['x-icode'] = '66AD1A8D605F606C'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import { Row, Col, Typography } from 'antd'
+import { Row, Col, Typography, Spin } from 'antd'
 import { withTranslation, WithTranslation } from 'react-i18next'
+import axios from 'axios'
 
 import {
   Header,
@@ -12,15 +13,70 @@ import {
   BusinessPartners
 } from '../../components/index'
 import styles from './HomePage.module.css'
-import { productList1, productList2, productList3 } from './mockups'
 import sideImage from '../../assets/images/sider_2019_12-09.png'
 import sideImage2 from '../../assets/images/sider_2019_02-04.png'
 import sideImage3 from '../../assets/images/sider_2019_02-04-2.png'
-import { t } from 'i18next'
 
-class HomePageComponent extends React.Component<WithTranslation> {
+interface State {
+  loading: boolean;
+  error: string | null;
+  productList: any[];
+}
+
+class HomePageComponent extends React.Component<WithTranslation, State> {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      loading: true,
+      error: null,
+      productList: []
+    }
+  }
+
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get(
+        'http://123.56.149.216:8080/api/productCollections'
+        )
+
+      this.setState({
+        loading: false,
+        error: null,
+        productList: data
+      })
+    } catch (error) {
+      if (error instanceof Error) {
+        this.setState({
+          error: error.message,
+          loading: false
+        })
+      }
+    }
+
+  }
+
   render (): JSX.Element {
-    console.log(this.props.t)
+    const { t } = this.props
+    const { productList, loading, error } = this.state
+
+    if (loading) {
+      return <Spin
+        size='large'
+        style={{
+          marginTop: 200,
+          marginBottom: 200,
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          width: '100%'
+        }}
+      />
+    }
+
+    if (error) {
+      return <div>网站出错: {error}</div>
+    }
+
     return (
       <>
         <Header />
@@ -37,17 +93,17 @@ class HomePageComponent extends React.Component<WithTranslation> {
           <ProductCollection
             title={<Typography.Title level={3} type='warning'>{t('home_page.hot_recommended')}</Typography.Title>}
             sideImage={sideImage}
-            products={productList1}
+            products={productList[0].touristRoutes}
           />
           <ProductCollection
             title={<Typography.Title level={3} type='warning'>{t('home_page.new_arrival')}</Typography.Title>}
             sideImage={sideImage2}
-            products={productList2}
+            products={productList[1].touristRoutes}
           />
           <ProductCollection
             title={<Typography.Title level={3} type='warning'>{t('home_page.domestic_travel')}</Typography.Title>}
             sideImage={sideImage3}
-            products={productList3}
+            products={productList[2].touristRoutes}
           />
         </div>
         <BusinessPartners />
