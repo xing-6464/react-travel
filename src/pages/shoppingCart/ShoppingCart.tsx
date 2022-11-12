6ab677a3448a1fe6e50509de1fc301d53fc4a15e
xing-6464 <xing_6464@foxmail.com>
2022-11-12 13:49:32 +0800

    购物车页面初始化

--- src/pages/shoppingCart/ShoppingCart.tsx
+++ src/pages/shoppingCart/ShoppingCart.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
+import { Row, Col, Affix } from 'antd'
+
+import styles from './ShoppingCart.module.css'
+import { MainLayout } from '../../layouts/mainLayout'
+import { ProductList, PaymentCard } from '../../components'
+
 export const ShoppingCart: React.FC = (props) => {
   return (
-    <div>
-      <h1>ShoppingCart</h1>
-    </div>
+    <MainLayout>
+      <Row>
+        {/* 购物车清单 */}
+        <Col span={16}>
+          <div className={styles['product-list-container']}>
+            {/* <ProductList /> */}
+          </div>
+        </Col>
+        {/* 支付卡组件 */}
+        <Col span={8}>
+          <Affix>
+            <div className={styles['payment-card-container']}>
+              {/* <PaymentCard /> */}
+            </div>
+          </Affix>
+        </Col>
+      </Row>
+    </MainLayout>
   )
 }
